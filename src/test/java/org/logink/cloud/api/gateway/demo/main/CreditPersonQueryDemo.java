e33fd8e64b5e0ab43a94233e1e83ce3f0f9bee04
xuzw <xuzw@123.com>
2019-04-17 11:50:31 +0800

    信用人员查询demo注释更新

--- src/test/java/org/logink/cloud/api/gateway/demo/main/CreditPersonQueryDemo.java
+++ src/test/java/org/logink/cloud/api/gateway/demo/main/CreditPersonQueryDemo.java
@@ -5,10 +5,11 @@ import org.logink.cloud.api.gateway.demo.Response;
 import org.logink.cloud.api.gateway.demo.util.PostUtil;
 
 /**
- * @ClassName: CreditPersonQueryDemo
- * @Description: 信用从业人员查询demo
- * @author: Xu Zhengwen
- * @date: 2019/4/8 14:38
+ * logink cloud gateway api demo
+ * 人员查询接口
+ * 重要提示如下：
+ * 代码请从 github path:https://github.com/loginkhub/api-gateway-demo-sign-java-master 下载
+ * 对应测试类： /src/test/java/org/logink/cloud/api/gateway/demo/main/CreditPersonQueryDemo.java
  */
 public class CreditPersonQueryDemo {
     public static void main(String[] args) {
